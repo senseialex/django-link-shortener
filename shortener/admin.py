63ee38238621ce91263df55e5d1bb9faa214a063
avelazquezmU <drummeralex264@gmail.com>
2020-04-08 12:54:56 -0500

    change admin configuration

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import UrlMap, UrlProfile
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 # Register your models here.
 admin.site.register(UrlMap)
 admin.site.register(UrlProfile)
+
+
+class UrlMapResource(resources.ModelResource):
+    class Meta:
+        model = UrlMap
+
+
+@admin.register(UrlMap)
+class documentosAdmin(ImportExportModelAdmin):
+    resource_class = UrlMapResource
+    list_display = ('user', 'full_url')
+    search_fields = ('full_url')
